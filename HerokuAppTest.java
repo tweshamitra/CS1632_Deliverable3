78134d16f227e88222f29d72db57b7ca7224b6a2
Twesha Mitra <twesha95@gmail.com>
2017-10-23 10:13:18 -0400

    added a test

--- HerokuAppTest.java
+++ HerokuAppTest.java
@@ -8,4 +8,21 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HerokuAppTest{
     static WebDriver driver = new HtmlUnitDriver();
+
+    @Before
+    public void setup(){
+        driver.get("https://cs1632ex.herokuapp.com/");
+    }
+    @Test
+    public void Test(){
+        String title = driver.getTitle();
+        assertTrue(title.contains("CS1632"));
+    }
+
+    @Test
+    public void testWelcomeMsg(){
+        WebElement e = driver.findElement(By.className("lead"));
+        String msg = e.getText();
+        assertTrue(msg.contains("Welcome, friend,"));
+    }
 }
