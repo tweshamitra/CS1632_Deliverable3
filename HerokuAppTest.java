8de1eeee73ad01b233f5d96c737f81e0ffac6b3d
Tyler Bobik <shed412@gmail.com>
2017-10-23 21:39:57 -0400

    did fibonacci test for 5

--- HerokuAppTest.java
+++ HerokuAppTest.java
@@ -25,4 +25,27 @@ public class HerokuAppTest{
         String msg = e.getText();
         assertTrue(msg.contains("Welcome, friend,"));
     }
+
+
+    /**
+        This is the first test for number 5 of the requirements
+        We will go to the Fibonacci page and enter the value 
+        "sproing" and see if the value is 1
+    */
+    @Test
+    public void fibStrTest(){
+        WebElement fib = driver.findElement(By.linkText("Fibonacci"));
+        fib.click();
+
+        WebElement textBox = driver.findElement(By.id("tb1"));
+        textBox.sendKeys("sproing");
+
+        WebElement submit = driver.findElement(By.id("sub"));
+        submit.click();
+
+        WebElement textArea = driver.findElement(By.className("jumbotron"));
+        String actual = textArea.getText();
+
+        assertTrue(actual.contains("1!"));
+    }
 }
